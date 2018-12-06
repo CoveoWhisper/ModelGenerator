628f7143abd29aef4eb12d265e90589a77cf0254
Atlantis251 <allec12b@gmail.com>
2018-12-06 11:21:37 -0500

    inital commit of this repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Model Generator',
+    version='1',
+    packages=['generator'],
+    license='MIT',
+    long_description='Project to generate model for Whisper product'
+)
